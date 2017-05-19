840ba2e98d476f1b135d860a0e53a58d1c47ded6
赵七七 <1352726123@qq.com>
2017-05-19 20:39:34 +0800

    正则验证手机号

--- note.php
+++ note.php
@@ -12,3 +12,8 @@ php artisan make:controller CategoryController
 //2017.05.18问题
 //ueditor 怎么设置提高上传图片速度
 
+2017.05.19
+正则验证手机号
+ var mobile = 13527261231;
+ var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/; 
+ alert(myreg.test(mobile));
